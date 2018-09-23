487638e931495028eedbdff3800756a725f74afe
zhyonk <zhyonk@163.com>
2018-09-24 00:39:27 +0800

    再次重构，rpc功能划分更清晰

---
+++ user-center/src/main/java/cn/zhyonk/user/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package cn.zhyonk.user.mapper;
+
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository; 
+
+import cn.zhyonk.common.SuperMapper;
+import cn.zhyonk.entity.User;
+
+/**
+ *
+ * User 表数据库控制层接口
+ *
+ */
+public interface UserMapper extends SuperMapper<User> {
+
+	String checkLogin(@Param(value = "phone") String phone,@Param(value = "password") String password);
+
+	User selectByOprnId(@Param(value = "openid") String uid);
+
+}
