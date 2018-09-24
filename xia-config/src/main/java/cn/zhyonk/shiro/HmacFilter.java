256fc0c9f5cbe0bab25bcfa188538c09aa6cc4be
zhyonk <zhyonk@163.com>
2018-09-24 23:20:31 +0800

    init

--- xia-config/src/main/java/cn/zhyonk/shiro/HmacFilter.java
+++ xia-config/src/main/java/cn/zhyonk/shiro/HmacFilter.java
@@ -49,9 +49,17 @@ public class HmacFilter extends AccessControlFilter {
 	 */
 	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
 		if (isHmacSubmission(request)) {// 如果是Hmac鉴权的请求
-			// 创建令牌
-			AuthenticationToken token = createToken(request, response);
+			String tokenStr = request.getParameter("token");
+			if(tokenStr==null) {
+				response.setCharacterEncoding("utf-8");
+				ResponseVO responseVO = LoginResponseCode
+						.buildEnumResponseVO(LoginResponseCode.USERID_NOT_UNAUTHORIZED, false);
+				responseMessage((HttpServletResponse) response, response.getWriter(), responseVO);
+			}
 			try {
+				
+				// 创建令牌
+				AuthenticationToken token = createToken(request, response);
 				Subject subject = getSubject(request, response);
 				subject.login(token);// 认证
 				return true;// 认证成功，过滤器链继续
@@ -64,7 +72,7 @@ public class HmacFilter extends AccessControlFilter {
 			} catch (ExpiredCredentialsException e) {
 				response.setCharacterEncoding("utf-8");
 				ResponseVO responseVO = LoginResponseCode
-						.buildEnumResponseVO(LoginResponseCode.LOGIN_TOKEN_NOT_NULL, false);
+						.buildEnumResponseVO(LoginResponseCode.LOGIN_TIME_EXP, false);
 				responseMessage((HttpServletResponse) response, response.getWriter(), responseVO);
 			} catch (UnknownAccountException e) {
 				response.setCharacterEncoding("utf-8");
@@ -91,11 +99,11 @@ public class HmacFilter extends AccessControlFilter {
 
 	protected boolean isHmacSubmission(ServletRequest request) {
 		String token = request.getParameter("token");
+		if(token==null) {
+			return true;
+		}
 		Login login = JWT.unsign(token, Login.class);
-		Map<String, Object> claims = JWT.getClaims(token);
-		Long maxAge = (Long) claims.get("exp");
-		return (request instanceof ServletRequest) && StringUtils.isNotBlank(login.getUid()) && StringUtils.isNotBlank(token)
-				&& StringUtils.isNotBlank(String.valueOf(maxAge));
+		return (request instanceof ServletRequest) && StringUtils.isNotBlank(login.getUid()) && StringUtils.isNotBlank(token);
 	}
 
 	private void responseMessage(HttpServletResponse response, PrintWriter out, ResponseVO responseVO) {
