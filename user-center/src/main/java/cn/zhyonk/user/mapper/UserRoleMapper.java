61ab79c442c14e024a4695d9072386cf192b024b
zhyonk <zhyonk@163.com>
2018-10-24 23:28:27 +0800

    添加userRole相关代码

---
+++ user-center/src/main/java/cn/zhyonk/user/mapper/UserRoleMapper.java
@@ -0,0 +1,16 @@
+package cn.zhyonk.user.mapper;
+
+import cn.zhyonk.common.SuperMapper;
+import cn.zhyonk.entity.UserRole;
+
+/**
+ * <p>
+ * 用户-角色表 Mapper 接口
+ * </p>
+ *
+ * @author Yanghu
+ * @since 2018-10-22
+ */
+public interface UserRoleMapper extends SuperMapper<UserRole> {
+
+}
---
+++ xia-api/src/main/java/cn/zhyonk/rpc/api/UserRoleService.java
@@ -0,0 +1,16 @@
+package cn.zhyonk.rpc.api;
+
+import cn.zhyonk.entity.UserRole;
+import com.baomidou.mybatisplus.service.IService;
+
+/**
+ * <p>
+ * 用户-角色表 服务类
+ * </p>
+ *
+ * @author Yanghu
+ * @since 2018-10-22
+ */
+public interface UserRoleService extends IService<UserRole> {
+
+}
