634946b66cd20b918940a8067cc8b736385c05a1
zhyonk <zhyonk@163.com>
2018-10-27 22:49:18 +0800

    Position相关的dao接口

---
+++ user-center/src/main/java/cn/zhyonk/user/mapper/PositionMapper.java
@@ -0,0 +1,16 @@
+package cn.zhyonk.user.mapper;
+
+import cn.zhyonk.entity.Position;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 职务表 Mapper 接口
+ * </p>
+ *
+ * @author Yanghu
+ * @since 2018-10-27
+ */
+public interface PositionMapper extends BaseMapper<Position> {
+
+}
